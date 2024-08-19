67998d268a68a1eca67ef39d56294df4d5684279
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-08-19 16:01:16 +0200

    recupero elemento dal dom e raccolta dati

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 // Esercizio dadi
 console.log('JS OK')
+
+// 1.recupero l'elemento dal dom
+const target = document.getElementById('target');
+console.log('id', target)
+
+// 2.raccolta dati
+const max = 6;
+const userPlay = Math.floor(Math.random() * max) + 1;
+console.log("giocata dell'utente", userPlay);
+
+const cpuPlay = Math.floor(Math.random() * max) + 1;
+console.log("giocata del computer", cpuPlay);
